93f437bf274a18ffa8436060819388e1e6555643
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-12-14 14:23:54 +0800

    fix: sidebar constant data menu

--- config/sidebar.php
+++ config/sidebar.php
@@ -13,41 +13,12 @@ return [
     // SISWA
 
     [
-        'title' => 'Kehadiran Anda',
-        'icon' => 'calendar-check',
-        'route-name' => 'presence-student.index',
-        'is-active' => 'presence-student.index',
-        'description' => 'Untuk melihat kehadiran anda.',
-        'roles' => ['siswa'],
-    ],
-
-    [
         'title' => 'Jadwal Mapel',
         'icon' => 'calendar-alt',
         'route-name' => 'schedule-student.index',
         'is-active' => 'schedule-student.index',
         'description' => 'Untuk melihat jadwal mapel anda.',
         'roles' => ['siswa'],
-    ],
-
-    // GUARDIAN
-
-    [
-        'title' => 'Kehadiran Anak Anda',
-        'icon' => 'calendar-check',
-        'route-name' => 'guardian-student-presence.index',
-        'is-active' => 'guardian-student-presence.index',
-        'description' => 'Untuk melihat kehadiran anda.',
-        'roles' => ['wali siswa'],
-    ],
-
-    [
-        'title' => 'Jadwal Mapel Anak Anda',
-        'icon' => 'calendar-alt',
-        'route-name' => 'guardian-student-schedule.index',
-        'is-active' => 'guardian-student-schedule.index',
-        'description' => 'Untuk melihat jadwal mapel anda.',
-        'roles' => ['wali siswa'],
     ],
 
     // GURU
@@ -58,15 +29,6 @@ return [
         'route-name' => 'schedule-teacher.index',
         'is-active' => 'schedule-teacher*',
         'description' => 'Untuk melihat jadwal mengajar anda.',
-        'roles' => ['guru'],
-    ],
-
-    [
-        'title' => 'Presensi Kelas',
-        'icon' => 'address-card',
-        'route-name' => 'class-attendance.index',
-        'is-active' => 'class-attendance*',
-        'description' => 'Untuk melakukan presensi kelas.',
         'roles' => ['guru'],
     ],
 
@@ -141,80 +103,6 @@ return [
     ],
 
     [
-        'title' => 'Wali Siswa',
-        'icon' => 'user-friends',
-        'route-name' => 'guardian-student.index',
-        'is-active' => 'guardian-student*',
-        'description' => 'Melihat daftar wali siswa.',
-        'roles' => ['admin', 'developer'],
-    ],
-
-    [
-        'title' => 'Qr Code',
-        'icon' => 'qrcode',
-        'route-name' => 'qrcode.index',
-        'is-active' => 'qrcode*',
-        'description' => 'Melihat daftar qr code.',
-        'roles' => ['admin', 'developer'],
-    ],
-
-    [
-        'title' => 'Scan Qr',
-        'icon' => 'clock',
-        'route-name' => 'scan-qr.index',
-        'is-active' => 'scan-qr*',
-        'description' => 'Untuk Scan Qr Code',
-        'roles' => ['admin', 'developer', 'operator'],
-    ],
-
-    // [
-    //     'title' => 'Whatsapp Broadcast',
-    //     'icon' => 'whatsapp',
-    //     'brand_icon' => true,
-    //     'route-name' => 'whatsapp-broadcast.index',
-    //     'is-active' => 'whatsapp-broadcast*',
-    //     'description' => 'Pengaturan whatsapp broadcast.',
-    //     'roles' => ['admin'],
-    // ],
-
-    [
-        'title' => 'Presensi',
-        'description' => 'Menampilkan daftar presensi pada aplikasi.',
-        'icon' => 'calendar-check',
-        'route-name' => 'attendance.class.index',
-        'is-active' => 'attendance*',
-        'roles' => ['admin', 'developer'],
-        'sub-menus' => [
-            [
-                'title' => 'Presensi Kelas',
-                'description' => 'Melihat daftar presensi kelas.',
-                'route-name' => 'attendance.class.index',
-                'is-active' => 'attendance.class*',
-            ],
-            [
-                'title' => 'Presensi Qrcode',
-                'description' => 'Melihat daftar presensi Qr Code.',
-                'route-name' => 'attendance.qrcode.check-in',
-                'is-active' => 'attendance.qrcode*',
-                'sub-menus' => [
-                    [
-                        'title' => 'Masuk',
-                        'description' => 'Melihat daftar presensi masuk.',
-                        'route-name' => 'attendance.qrcode.check-in',
-                        'is-active' => 'attendance.qrcode.check-in',
-                    ],
-                    [
-                        'title' => 'Keluar',
-                        'description' => 'Melihat daftar presensi keluar.',
-                        'route-name' => 'attendance.qrcode.check-out',
-                        'is-active' => 'attendance.qrcode.check-out',
-                    ],
-                ],
-            ],
-        ],
-    ],
-
-    [
         'title' => 'Laporan',
         'description' => 'Menampilkan daftar laporan pada aplikasi.',
         'icon' => 'print',
@@ -245,32 +133,6 @@ return [
                 'description' => 'Melihat daftar jadwal kelas.',
                 'route-name' => 'report.class-schedule.index',
                 'is-active' => 'report.class-schedule*',
-            ],
-            [
-                'title' => 'Presensi Kelas',
-                'description' => 'Melihat daftar presensi kelas.',
-                'route-name' => 'report.attendance.class.index',
-                'is-active' => 'report.attendance.class*',
-            ],
-            [
-                'title' => 'Presensi Qrcode',
-                'description' => 'Melihat daftar presensi Qr Code.',
-                'route-name' => 'report.attendance.qrcode.check-in',
-                'is-active' => 'report.attendance.qrcode*',
-                'sub-menus' => [
-                    [
-                        'title' => 'Presensi Masuk',
-                        'description' => 'Melihat daftar presensi masuk.',
-                        'route-name' => 'report.attendance.qrcode.check-in',
-                        'is-active' => 'report.attendance.qrcode.check-in',
-                    ],
-                    [
-                        'title' => 'Presensi Keluar',
-                        'description' => 'Melihat daftar presensi keluar.',
-                        'route-name' => 'report.attendance.qrcode.check-out',
-                        'is-active' => 'report.attendance.qrcode.check-out',
-                    ],
-                ],
             ],
         ],
     ],
